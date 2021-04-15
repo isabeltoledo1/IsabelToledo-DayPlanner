a59f12f6efc89993d1b51f4e170350fa20f34f38
Isabel Toledo <isabeltoledo9@gmail.com>
2021-04-14 19:44:01 -0600

    added onCLick for saveBtn

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 function timeUpdate() {
+  const currentHour = parseInt(moment().format("kk"));
+  const currentDayEl = document.getElementById("currentDay");
 
-    const currentHour = parseInt(moment().format("kk"));
-    const currentDayEl = document.getElementById("currentDay");
+  //sets current time
+  currentDayEl.innerHTML = moment().format("dddd, MMMM Do YYYY");
 
-    //sets current time
-    currentDayEl.innerHTML = moment().format("dddd, MMMM Do YYYY"); 
-
-    //loops over time blocks to update css
-    $('.time-block').each(function () {
-        var blockHour = parseInt($(this).attr('id').split('-')[1]);
-        //check if we have moved past this time 
-        if (blockHour <currentHour) {
-          $(this).addClass('past');
-        } else if (blockHour === currentHour) {
-          $(this).removeClass('past');
-          $(this).addClass('present');
-        } else {
-          $(this).removeClass('past');
-          $(this).removeClass('present');
-          $(this).addClass('future');
-        }
-      });
+  //loops over time blocks to update css
+  $(".time-block").each(function () {
+    var hour = parseInt($(this).attr("id").split("-")[1]);
+    //check if we have moved past this time
+    if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
     }
+  });
+
+  //onclick event to saveBtn
+  $(".saveBtn").on("click", function () {
+    var description = $(this).siblings(".description").val();
+    console.log(description);
+    var time = $(this).parent().attr("id");
+  
+ 
+    //adds to local storage
+      localStorage.setItem(time, appt);
+   
+  });
+
+
+
+
+}
 
 timeUpdate();
